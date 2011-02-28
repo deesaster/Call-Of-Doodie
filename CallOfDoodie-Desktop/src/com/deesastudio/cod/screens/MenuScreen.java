04d798b49bf842fd7a92eea201f680bfc055b292
Denis Smirnov <denis@deesastudio.com>
2011-02-27 18:17:24 -0700

    Added background images + code, remove 'bin' from tracking

--- CallOfDoodie-Desktop/src/com/deesastudio/cod/screens/MenuScreen.java
+++ CallOfDoodie-Desktop/src/com/deesastudio/cod/screens/MenuScreen.java
@@ -1,12 +1,23 @@
 package com.deesastudio.cod.screens;
 
 import com.badlogic.gdx.Application;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL10;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.Texture.TextureFilter;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Matrix4;
 
 public class MenuScreen implements Screen {
-
+    private final SpriteBatch     mSpriteBatch;
+    private final Texture         mBackgroundTexture;
+    private final Matrix4         mViewMatrix = new Matrix4();
+    private final Matrix4         mTransformMatrix = new Matrix4();
+    
     public MenuScreen(Application app) {
-        
+        mSpriteBatch = new SpriteBatch();
+        mBackgroundTexture = new Texture(Gdx.files.internal("graphics/background.png"));
+        mBackgroundTexture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
     }
     
     @Override
@@ -18,7 +29,14 @@ public class MenuScreen implements Screen {
     @Override
     public void render(Application app) {
         app.getGraphics().getGL10().glClear(GL10.GL_COLOR_BUFFER_BIT);
-
+        
+        mViewMatrix.setToOrtho2D(0, 0, 480, 320);
+        mSpriteBatch.setProjectionMatrix(mViewMatrix);
+        mSpriteBatch.setTransformMatrix(mTransformMatrix);
+        mSpriteBatch.begin();
+        mSpriteBatch.disableBlending();
+        mSpriteBatch.draw(mBackgroundTexture, 0, 0, 480, 320, 0, 0, 480, 320, false, false);
+        mSpriteBatch.end();
     }
 
     @Override
